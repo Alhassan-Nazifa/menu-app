800cf3e9bfebef3358e0ecb5b7305225a665e498
Alhassan-Nazifa <naalhassan174@gmail.com>
2022-04-27 07:21:05 -0700

    FIRST PUSH

---
+++ src/screens/Login.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Router from "router";
+import {Link} from "react-router-dom"
+
+function Login() {
+  return (
+    <form className="form justify-content-md-center">
+      <div className="col-sm-12 col-md-6 col-1g-4 col-x1-4 m-3 p-3 bg-info">
+        <div className="form-group m-2">
+          <label htmlFor="username">username</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter username"
+          />
+        </div>
+        <div className="form-group m-2">
+          <label htmlFor="email">email</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="form-group m-2">
+          <label htmlFor="password">password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+              </div>
+              <div>
+                  I don't have an account
+                  <Link to="/register" className="nav-link">Register</Link>
+              </div>
+          
+        
+      </div>
+    </form>
+  );
+}
+
+export default Login;
